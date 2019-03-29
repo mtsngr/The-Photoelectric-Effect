8e0f2beb99d41696cda05c8380a321632047f8f5
Muhammet Taha Sungur <47477604+mtsngr@users.noreply.github.com>
2019-03-29 16:42:11 +0300

    Add files via upload

---
+++ green2.C
@@ -0,0 +1,74 @@
+{
+
+// Line fitting macro written for ROOT
+
+//      GREEN
+
+  const int ndata = 3;
+
+  float x[ndata] = {0.550, 0.580, 0.600};
+  float y[ndata] = {-0.04, -0.06, -0.08};
+  float sx[ndata]= {0.001, 0.001, 0.001};
+  float sy[ndata]= {0.02, 0.02, 0.02};
+
+
+  float weight = 0;
+  float totw = 0; // Total weight
+  float xybar = 0, xbar = 0, ybar = 0, x2bar = 0; // weighted averages
+
+  for (int i=0; i<ndata; ++i) {
+    weight = 1./(sy[i]*sy[i]);
+    totw += weight;
+    xybar += (x[i]*y[i]*weight);
+    xbar += (x[i]*weight);
+    ybar += (y[i]*weight);
+    x2bar += (x[i]*x[i]*weight);
+  }
+
+  xybar /= totw;
+  xbar /= totw;
+  ybar /= totw;
+  x2bar /= totw;
+
+  float sy2bar = ndata / totw; // weighted average error squared
+  float slope = (xybar - xbar*ybar) / (x2bar - xbar*xbar);
+  float itcpt = ybar - slope * xbar;
+  float slopeerr = sqrt ( sy2bar / (ndata * (x2bar - xbar*xbar) ) );
+  float itcpterr = sqrt ( x2bar ) * slopeerr;
+  cout << "slope of fit line = " << slope << " +- " << slopeerr << endl;
+  cout << "intercept of fit line = " << itcpt << " +- " << itcpterr << endl;
+
+
+  TGraphErrors *mygraph = new TGraphErrors(ndata,x,y,sx,sy);
+  mygraph->Draw("A*");
+
+  /*
+  TF1 *ffitline = new TF1("ffitline","[0]*x+[1]",0,6); // x ekseninin aralığı [0,6]
+  ffitline->SetParameter(0,slope);
+  ffitline->SetParameter(1,itcpt);
+  ffitline->SetLineColor(kBlue); // draw in blue color
+  ffitline->SetLineStyle(2); // draw dotted line
+  //ffitline->Draw("same");
+  */
+
+  mygraph->SetTitle("; Volt (V); Current with unit of (10^{-13}A)");
+
+
+  // let's also try the same with ROOT's own fitter
+  TF1 *fnew = new TF1("fnew","[0]*x+[1]",0,6);
+  fnew->SetParameters(3.1416,2.7182); // arbitrary starting parameters
+  mygraph->Fit(fnew);
+  mygraph->Draw("A*");
+  
+
+  gStyle->SetOptStat(111110);
+  gStyle->SetOptFit(1111);
+
+
+
+}
+
+
+
+
+
---
+++ yellow1.C
@@ -0,0 +1,73 @@
+// Line fitting macro written for ROOT
+{
+
+//       YELLOW
+
+  const int ndata = 6;
+
+  float x[ndata] = {0.100, 0.150, 0.200, 0.250, 0.300, 0.350};
+  float y[ndata] = {2.8, 2.0, 1.6, 1.0, 0.8, 0.4};
+  float sx[ndata]= {0.001, 0.001, 0.001 , 0.001 , 0.001, 0.001};
+  float sy[ndata]= {0.2, 0.2, 0.2, 0.2, 0.2, 0.2};
+
+
+  float weight = 0;
+  float totw = 0; // Total weight
+  float xybar = 0, xbar = 0, ybar = 0, x2bar = 0; // weighted averages
+
+  for (int i=0; i<ndata; ++i) {
+    weight = 1./(sy[i]*sy[i]);
+    totw += weight;
+    xybar += (x[i]*y[i]*weight);
+    xbar += (x[i]*weight);
+    ybar += (y[i]*weight);
+    x2bar += (x[i]*x[i]*weight);
+  }
+
+  xybar /= totw;
+  xbar /= totw;
+  ybar /= totw;
+  x2bar /= totw;
+
+  float sy2bar = ndata / totw; // weighted average error squared
+  float slope = (xybar - xbar*ybar) / (x2bar - xbar*xbar);
+  float itcpt = ybar - slope * xbar;
+  float slopeerr = sqrt ( sy2bar / (ndata * (x2bar - xbar*xbar) ) );
+  float itcpterr = sqrt ( x2bar ) * slopeerr;
+  cout << "slope of fit line = " << slope << " +- " << slopeerr << endl;
+  cout << "intercept of fit line = " << itcpt << " +- " << itcpterr << endl;
+
+
+  TGraphErrors *mygraph = new TGraphErrors(ndata,x,y,sx,sy);
+  mygraph->Draw("A*");
+
+  /*
+  TF1 *ffitline = new TF1("ffitline","[0]*x+[1]",0,6); // x ekseninin aralığı [0,6]
+  ffitline->SetParameter(0,slope);
+  ffitline->SetParameter(1,itcpt);
+  ffitline->SetLineColor(kBlue); // draw in blue color
+  ffitline->SetLineStyle(2); // draw dotted line
+  //ffitline->Draw("same");
+  */
+
+  mygraph->SetTitle("; Volt (V); Current with unit of (10^{-13}A)");
+
+
+  // let's also try the same with ROOT's own fitter
+  TF1 *fnew = new TF1("fnew","[0]*x+[1]",0,6);
+  fnew->SetParameters(3.1416,2.7182); // arbitrary starting parameters
+  mygraph->Fit(fnew);
+  mygraph->Draw("A*");
+  
+
+  gStyle->SetOptStat(111110);
+  gStyle->SetOptFit(1111);
+
+
+
+}
+
+
+
+
+
